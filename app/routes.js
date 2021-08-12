eb6e4c98872d2e4bb183a9dc4fecfd502184808a
Tasnya <tasnya.khanom@kainos.com>
2021-08-12 15:42:07 +0100

    add department field

--- app/routes.js
+++ app/routes.js
@@ -4,51 +4,66 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 const employeedata = require('./employeedata.js')
 
-router.get('/list-employees', async (req, res) => { 
-    res.render('list-employees', { employees: await employeedata.getEmployees() } ) 
+router.get('/list-employees', async (req, res) => {
+    res.render('list-employees', { employees: await employeedata.getEmployees() })
 });
 
 router.get('/list-employees/:substr', (req, res) => {
-    res.render('list-employees', 
-    { 
-      namefilter: req.params.substr.toLowerCase(), 
-      employees: employeedata.getEmployees() 
-   }); 
+    res.render('list-employees',
+        {
+            namefilter: req.params.substr.toLowerCase(),
+            employees: employeedata.getEmployees()
+        });
 });
 
-router.get('/addemployees', async (req, res) => { 
-    res.render('newempform', { employees: await employeedata.getEmployees() } ); 
-    });
+router.get('/addemployee', async (req, res) => {
+    res.render('newempform', { employees: await employeedata.getEmployees() });
+});
 
-router.post('/addemployees', async (req, res) => { 
+router.post('/addemployee', async (req, res) => {
     var emp = req.body;
+    //res.locals.errormessage = "";
     let validityFlag = true;
-
-    
-    if(hasValidInputLength(emp.emp_name, 50)){
-        
-        return;
+    if (!hasValidInputLength(emp.emp_name, 50)) {
+        res.locals.errormessage = "Name is too long (50 chars max)"
+        console.error("ERROR on name")
+        validityFlag = false;
     }
 
-
-    validityFlag = hasValidInputLength(emp.emp_address, 100)
-    validityFlag = isValidIban(emp.ninum);
-
-    if(!validityFlag){
-        return
+    if (!hasValidInputLength(emp.emp_address, 100)) {
+        res.locals.errormessage = "Address is too long (100 chars max)"
+        console.error("ERROR on address")
+        validityFlag = false;
     }
 
-    // validate here 
-    let insertedKey = await employeedata.addEmployee(req.body)
-    res.render('list-employees', { employees: await employeedata.getEmployees()} );
+    if (!hasValidInputLength(emp.ninum.replace(/\s/g, ""), 13)) {
+        res.locals.errormessage = "NIN is not valid (13 chars max)"
+        console.error("ERROR on NIN")
+        validityFlag = false;
+    }
+
+    if (!isValidIban(emp.iban)) {
+        res.locals.errormessage = "IBAN is not valid"
+        console.error("ERROR on IBAN")
+        validityFlag = false;
+    }
+
+    if (!validityFlag) {
+        console.error("Validity flag is not valid :C")
+        res.render('newempform', { employees: req.body });
+    } else {
+        console.log(req.body)
+        let insertedKey = await employeedata.addEmployee(req.body)
+        res.render('list-employees', { employees: await employeedata.getEmployees() });
+    }
 })
 
-function hasValidInputLength(value, maxlength){
-    return value.length > maxlength;
+function hasValidInputLength(value, maxlength) {
+    return value.length < maxlength;
 }
 
-function isValidIban(iban){
-    return hasValidInputLength(iban, 34) && iban.charAt(0).isNaN() && iban.charAt(1).isNan();
+function isValidIban(iban) {
+    return hasValidInputLength(iban, 34) && isNaN(iban.charAt(0)) && isNaN(iban.charAt(1));
 }
 
 module.exports = router
